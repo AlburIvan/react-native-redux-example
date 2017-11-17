cf6835320318f48e3781c8c020098cd8f4cd6f8f
AlburIvan <albur.ivan@outlook.com>
2017-11-17 02:25:18 -0400

    Finished Section 11 & 12

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import  { View, Text, StyleSheet } from 'react-native';
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducers";
 
 import { Header } from "./components/common";
+import LibraryList from "./components/LibraryList";
 
 
 export default class App extends Component {
@@ -13,6 +14,7 @@ export default class App extends Component {
         <Provider store={createStore(reducers)}>
           <View>
             <Header headerText="Tech Stack" />
+            <LibraryList />
           </View>
         </Provider>
       );
